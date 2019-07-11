6dba8c3beb2a8090a637db357c8aa3dd22ec8a1e
Harsh Rajat <ProjectHydro@3602652.no-reply.drupal.org>
2019-07-11 13:43:41 +0530

    HydroPay with GWEI Restrictions

--- lambda-src/provider.js
+++ lambda-src/provider.js
@@ -39,7 +39,7 @@ export async function handler(event) {
       to: to,
       data: transactionData,
       nonce: nonceTx,
-      gasPrice: gasPrice,
+      //gasPrice: gasPrice,
     };
 
     const transaction = await wallet.sendTransaction(tx);
